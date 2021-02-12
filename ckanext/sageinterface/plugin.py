0207fea92509ee74598cf22f4ba06d0051e09ded
iperezx <i3perez@sdsc.edu>
2021-02-12 19:46:53 +0000

    Add recline with grid view

--- ckanext/sageinterface/plugin.py
+++ ckanext/sageinterface/plugin.py
@@ -19,19 +19,9 @@ class SageinterfacePlugin(p.SingletonPlugin):
         p.toolkit.add_public_directory(config_, 'public')
         p.toolkit.add_resource('public', 'ckanext-sageinterface')
         self.proxy_enabled = p.plugin_loaded('resource_proxy')
-        self.text_formats = ['text/plain', 'txt', 'plain']
-        self.xml_formats = ['xml', 'rdf', 'rdf+xml', 'owl+xml', 'atom', 'rss']
-        self.json_formats = ['json']
-        self.jsonp_formats = ['jsonp']
 
     def can_view(self, data_dict): 
         # IResourceView
-        # Make it a requirement that I need to add resource_proxy to my plugins
-        # So this can work
-        resource = data_dict['resource']
-        url,archived = get_url(resource)
-        print url
-        print archived
         datastoreActive = data_dict['resource'].get('datastore_active', False)
         log.info('can_view::proxy_enabled: {0}'.format(self.proxy_enabled))
         log.info('can_view::datastoreActive: {0}'.format(datastoreActive))
@@ -52,7 +42,6 @@ class SageinterfacePlugin(p.SingletonPlugin):
     
     def info(self):
         # IResourceView 
-        # Add another option to the Views section called Data view with a new icon
         name = 'sageinterfaceview'
         title = 'Data View'
         viewConfig = {'name': name,'title':title,'always_available': True, 'default_title': title, 'icon': 'server'}
@@ -60,36 +49,28 @@ class SageinterfacePlugin(p.SingletonPlugin):
     
     def can_view(self, data_dict):
         # IResourceView
-        # Make it a requirement that I need to add resource_proxy to my plugins
-        # So this can work
         proxy_enabled = p.plugin_loaded('resource_proxy')
         datastore_active = data_dict['resource'].get('datastore_active', False)
         log.info('proxy_enabled: {0}'.format(self.proxy_enabled))
         log.info('datastoreActive: {0}'.format(datastore_active))
         return self.proxy_enabled and not datastore_active
     
-    def setup_template_variables(self, context, data_dict):
-        metadata = {'text_formats': self.text_formats,
-                    'json_formats': self.json_formats,
-                    'jsonp_formats': self.jsonp_formats,
-                    'xml_formats': self.xml_formats}
-        
+    def setup_template_variables(self, context, data_dict):        
         package_name = data_dict['package']['name']
         resource = data_dict['resource']
         query = dict()
         url,archived = get_url(resource,package_name,query)
         if not archived:
             url = proxy.get_proxified_resource_url(data_dict)
-            format_lower = data_dict['resource']['format'].lower()
-            if format_lower in self.jsonp_formats:
-                url = data_dict['resource']['url']
-        
-        return {'preview_metadata': json.dumps(metadata),
-                'resource_json': json.dumps(data_dict['resource']),
+
+        log.info('view: {0}'.format(json.dumps(data_dict['resource_view'])))
+        log.info('url: {0}'.format(json.dumps(url)))
+        return {'resource_view_json': json.dumps(data_dict['resource_view']),
                 'resource_url': json.dumps(url)}
 
     def preview_template(self, context, data_dict):
-        return 'sageinterface.html'
+        return 'sageinterface_recline.html'
 
     def view_template(self, context, data_dict):
-        return 'sageinterface.html'
+        return 'sageinterface_recline.html'
+    
---
+++ ckanext/sageinterface/public/sageinterface_recline.js
@@ -0,0 +1,103 @@
+ckan.module('sageinterface_recline', function (jQuery) {
+  return {
+    options: {
+        controlsClassName: "controls"
+    },
+    initialize: function () {
+        jQuery.proxyAll(this, /_on/);
+        this.options.resourceUrl = JSON.parse(this.options.resourceUrl)
+        this.options.resourceView = JSON.parse(this.options.resourceView);
+        this.el.ready(this._onReady);
+    },
+    _onReady: function() {
+        var resourceURL = this.options.resourceUrl,
+            resourceView = this.options.resourceView;
+  
+        this.loadView(resourceURL, resourceView);
+    },
+    loadView: function (resourceURL, reclineView) {
+        var self = this;
+        function showError(msg){
+          msg = msg || self._('error loading view');
+          window.parent.ckan.pubsub.publish('data-viewer-error', msg);
+        }
+  
+        var errorMsg, dataset, map_config,resourceData;
+        var request = new XMLHttpRequest();
+        request.open('GET',resourceURL,false);
+        request.send();
+        resourceData = JSON.parse(request.response);
+        console.log(resourceData);
+        dataset = new recline.Model.Dataset({records: resourceData});
+  
+        var query = new recline.Model.Query();
+        query.set({ size: reclineView.limit || 100 });
+        query.set({ from: reclineView.offset || 0 });
+  
+        var urlFilters = {};
+        try {
+          if (window.parent.ckan.views && window.parent.ckan.views.filters) {
+            urlFilters = window.parent.ckan.views.filters.get();
+          }
+        } catch(e) {}
+        
+        dataset.queryState.set(query.toJSON(), {silent: true});
+  
+        errorMsg = this._('Could not load view for sage interface');
+        dataset.fetch()
+          .done(function(dataset){
+              self.initializeView(dataset);
+          })
+          .fail(function(error){
+            if (error.message) errorMsg += ' (' + error.message + ')';
+            showError(errorMsg);
+          });
+    },
+    initializeView: function (dataset) {
+        var view = this._newDataExplorer(dataset);
+        view.visible = true;
+        view.render();
+    },
+    _newDataExplorer: function (dataset) {
+      var views = [
+        {
+          id: 'grid',
+          label: this._('Grid'),
+          view: new recline.View.SlickGrid({
+            model: dataset
+          })
+        }
+      ];
+
+      var sidebarViews = [
+        {
+          id: 'valueFilter',
+          label: this._('Filters'),
+          view: new recline.View.ValueFilter({
+            model: dataset
+          })
+        }
+      ];
+
+      var dataExplorer = new recline.View.MultiView({
+        el: this.el,
+        model: dataset,
+        views: views,
+        sidebarViews: sidebarViews,
+        config: {
+          readOnly: true
+        }
+      });
+
+      return dataExplorer;
+    },
+
+    _renderControls: function (el, controls, className) {
+        var controlsEl = jQuery("<div class=\"clearfix " + className + "\" />");
+        for (var i = 0; i < controls.length; i++) {
+          controlsEl.append(controls[i].el);
+        }
+        jQuery(el).append(controlsEl);
+    }
+  };
+});
