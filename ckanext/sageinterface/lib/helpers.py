59ad6cfb390daee18abe796d9828d3f843d435eb
iperezx <i3perez@sdsc.edu>
2021-03-17 23:47:59 +0000

    Update to support query from sage data api and backwards compatible with previous format.

--- ckanext/sageinterface/lib/helpers.py
+++ ckanext/sageinterface/lib/helpers.py
@@ -5,6 +5,7 @@ import urllib
 import urllib2
 import logging
 import json
+import ssl
 from pylons import config
 from requests import Session, Request
 import ckan.model as model
@@ -14,6 +15,11 @@ import ckanext.resourceproxy.plugin as proxy
 from ckan.plugins import toolkit as tk
 
 log = logging.getLogger('ckanext.sageinterface.lib.helpers')
+sagecommons_formats = ['json']
+string_null = 'null'
+data_keyword = 'data'
+meta_keyword = 'metadata'
+honeyhouse_keyword='honeyhouse'
 
 def identify_resource(resource_id,dataset_name):
     '''Returns a printable identity of a resource object.
@@ -22,68 +28,44 @@ def identify_resource(resource_id,dataset_name):
     # FIX: add group name later
     return '/dataset/{0}/resource/{1}'.format(dataset_name, resource_id)
 
-def get_url(resource,dataset_name,query):
-    from requests.exceptions import InvalidURL
-
-    url = None
-    archived = False
-    query['mimetype'] = None
-    resource_id = resource.get('id')
-    direct_url = resource.get('url')
-    archival = Archival.get_for_resource(resource_id)
-    resource_url = identify_resource(resource_id,dataset_name)
-    
-    if archival:
-        # Look for a local cache of the data file
-        if archival.cache_filepath:
-            if os.path.exists(archival.cache_filepath.encode('utf8')):
-                log.debug('Previewing local cached data: %s', archival.cache_filepath)
-                url = archival.cache_filepath
-                archived = True
-            else:
-                log.debug('Local cached data file missing: %s', archival.cache_filepath)
+def get_rawdata(url,query):
+    context = ssl._create_unverified_context()
+    empty_dict = {data_keyword:[],meta_keyword:[]}
+    try:
+        if query == string_null:
+            req = urllib2.Request(url)
         else:
-            log.debug('No cache_filepath for resource %s', resource_url)
+            req = urllib2.Request(url,query)
+        r = urllib2.urlopen(req,context=context)
+    except Exception, e:
+        log.error(u"Request {0}, {1}".format(url, e))
+        return empty_dict
 
-        # Otherwise try the cache_url
-        if not url:
-            if archival.cache_url:
-                try:
-                    u = fix_url(archival.cache_url)
-                except InvalidURL:
-                    log.error("Unable to fix the URL for resource: %s" % resource_url)
-                    return None, False
-                try:
-                    req = urllib2.Request(u)
-                    req.get_method = lambda: 'HEAD'
-
-                    r = urllib2.urlopen(req)
-                    if r.getcode() == 200:
-                        url = u
-                        query['length'] = r.info().get("content-length", 0)
-                        query['mimetype'] = r.info().get('content-type', None)
-                        log.debug('Previewing cache URL: %s', url)
-                except Exception, e:
-                    log.error(u"Request {0} with cache url {1}, {2}".format(resource_url, u, e))
-            else:
-                log.debug('No cache_url for resource %s', resource_url)
+    data_str = r.read()
+    if honeyhouse_keyword in url:
+        data_json = [json.loads(str(item)) for item in data_str.strip().split('\n')]
     else:
-        log.debug('Resource is not archived: %s', resource_url)
+        data_json = json.loads(data_str)
     
-    return url,archived
+    if type(data_json) == dict:
+        if data_keyword in data_json.keys() and meta_keyword in data_json.keys():
+            data = {data_keyword: data_json[data_keyword],
+                    meta_keyword:data_json[meta_keyword]}
+        else:
+            return empty_dict
+    else:
+        data = {data_keyword: data_json,meta_keyword:[]}
+    return data
+
+
 
 def get_data(data_dict):
     package_name = data_dict['package']['name']
     resource = data_dict['resource']
-    query = dict()
-    url,archived = get_url(resource,package_name,query)
-    if not archived:
-        url = resource.get('url')
-    req = urllib2.Request(url)
-    r = urllib2.urlopen(req)
-    data_str = r.read()
-    data_json = json.loads(data_str)
-    return data_json
+    query = json.dumps(resource.get('query'))
+    url = resource.get('url')
+    data = get_rawdata(url,query)
+    return data
 
 def getFieldsTemplate():
     template = {
@@ -96,15 +78,12 @@ def getFieldsTemplate():
 
 def get_metadata(resource):
     datastore_active = resource.get('datastore_active')
-    formatType = resource.get('Format')
-    if not datastore_active and formatType == "JSON":
+    formatType = resource.get('format').lower()
+    if not datastore_active and formatType in sagecommons_formats:
         url = resource.get('url')
-        req = urllib2.Request(url)
-        r = urllib2.urlopen(req)
-        data_str = r.read()
-        data_json = json.loads(data_str)
-        metadata_key = "metadata"
-        if type(data_json) == dict:
-            if metadata_key in data_json.keys():
-                return data_json[metadata_key]
+        query = json.dumps(resource.get('query'))
+        data = get_rawdata(url,query)
+        if type(data) == dict:
+            if meta_keyword in data.keys():
+                return data[meta_keyword]
     return None
